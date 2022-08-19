74f28b876871219171ae132a324ac8feb379bafb
odizgb <odizgb@github.io>
2022-08-19 07:13:19 +0300

    Initiating API project

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+[ApiController]
+[Controller]
+[Route("Auth")]
+public class AuthController : ControllerBase {
+    
+    public AuthController()
+    {
+        
+    }
+
+}
